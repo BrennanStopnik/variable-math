ba0ea83e4ed98976690369c1432af5b7c0959dc5
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-20 12:37:50 -0400

    worked the adder file instructions

---
+++ adder.js
@@ -0,0 +1,14 @@
+/*
+Create two variables, num1 and num2. Store any numbers in the variables.
+
+Below this, add num1 and num2 together and store the result in a new variable (name it whatever you want).
+
+Finlly, log out num1, num2, and sum in a human readable sentence (i.e.: "8 and 5 add to 13").
+*/
+
+let num1 = 14;
+let num2 = 27;
+
+add_it_up = num1 + num2;
+
+console.log("Adding " + num1 + " to " + num2 + " equals " + add_it_up + ".");
--- future-age.js
+++ future-age.js
@@ -1,3 +1,8 @@
+/*
+Create a variable called age and set its value to your age. 
+Now, write another line of code that logs out a sentence telling you how old you will be in five years.
+*/
+
 let age = 42;
 age += 5;
 console.log("I will be " + age + " years old in 5 years.");
