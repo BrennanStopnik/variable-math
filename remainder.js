5e9ad7da1adba7283c6b6e02c70094c035c290a9
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-20 12:49:23 -0400

    Update remainder.js
    Using the mod operator.

---
+++ remainder.js
@@ -0,0 +1,10 @@
+/*
+Create two variables that each store a positive number (name them anything you want, any positive numbers will do).
+
+Log out how the remainder that is left over when the first number is divided by the second number (i.e.: "the remainder of 7 divided by 3 is 1")
+*/
+
+let a = 16;
+let b = 3;
+let c = (a % b);
+console.log("The remainder of " + a + " divided by " + b + " is " + c + ".");
