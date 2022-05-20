411b5bd9abe3ee9e27d87af2827730df61de968a
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-20 12:43:23 -0400

    Update greeter.js

---
+++ greeter.js
@@ -0,0 +1,8 @@
+/*
+Create a variable name and store your name in it.
+
+Below this, write a line of code that displays some greeting to that name (i.e.: "Hello Barry!")
+*/
+
+let name = "Brennan";
+console.log("Hey " + name + "! What the story morning glory?")
